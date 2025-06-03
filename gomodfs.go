301e5f08746d717c4afa5aa0ae7931ee84918076
Brad Fitzpatrick <bradfitz@tailscale.com>
2025-06-03 14:03:06 +0100

    kinda works
    Signed-off-by: Brad Fitzpatrick <bradfitz@tailscale.com>

--- gomodfs.go
+++ gomodfs.go
@@ -13,27 +13,20 @@ import (
 
 	"github.com/hanwen/go-fuse/v2/fs"
 	"github.com/hanwen/go-fuse/v2/fuse"
+	"github.com/tailscale/gomodfs/modgit"
 )
+
+type config struct {
+	Git *modgit.Downloader // or nil to use default client
+
+}
 
 type moduleNameNode struct {
 	fs.Inode
 	paths []string
+	conf  *config
 
 	didInit bool
-}
-
-type moduleRoot struct {
-	fs.Inode
-	modName string // "tailscale.com@v1.83.0-pre.0.20250508040513-5be6ff9b62ec"
-}
-
-func (n *moduleRoot) Lookup(ctx context.Context, name string, out *fuse.EntryOut) (*fs.Inode, syscall.Errno) {
-	if name != "info" {
-		return nil, syscall.ENOENT
-	}
-	return n.NewInode(ctx, &memFile{
-		contents: []byte("Module: " + n.modName + "\n"),
-	}, fs.StableAttr{}), 0
 }
 
 // Ensure that we implement NodeOnAdder
@@ -56,15 +49,21 @@ func (n *moduleNameNode) Lookup(ctx context.Context, name string, out *fuse.Entr
 		modName = rxCapital.ReplaceAllStringFunc(modName, func(in string) string {
 			return strings.ToUpper(in[1:])
 		})
-		in := n.Inode.NewInode(ctx, &moduleRoot{
-			modName: modName,
+
+		res, err := n.conf.Git.Get(ctx, modName)
+		if err != nil {
+			log.Printf("Failed to get module %q: %v", modName, err)
+			return nil, syscall.EIO
+		}
+		return n.NewInode(ctx, &treeNode{
+			conf: n.conf,
+			tree: res.Tree,
 		}, fs.StableAttr{
 			Mode: fuse.S_IFDIR | 0755,
-		})
-		return in, 0
-
+		}), 0
 	}
 	in := n.Inode.NewInode(ctx, &moduleNameNode{
+		conf:  n.conf,
 		paths: append(slices.Clone(n.paths), name),
 	}, fs.StableAttr{
 		Mode: fuse.S_IFDIR | 0755,
@@ -103,9 +102,76 @@ func (f *memFile) Getattr(ctx context.Context, h fs.FileHandle, out *fuse.AttrOu
 	return 0
 }
 
+type treeNode struct {
+	fs.Inode
+	conf *config
+	tree string
+}
+
+func gitType(dir, treeHash string, ent string) (string, error) {
+	cmd := exec.Command("git", "-C", dir, "cat-file", "-t", treeHash+":"+ent)
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		return "", err
+	}
+	return strings.TrimSpace(string(out)), nil
+}
+
+var _ fs.NodeLookuper = (*treeNode)(nil)
+
+func (n *treeNode) Lookup(ctx context.Context, name string, out *fuse.EntryOut) (*fs.Inode, syscall.Errno) {
+	typ, err := gitType(n.conf.Git.GitRepo, n.tree, name)
+	log.Printf("treeNode(%v).Lookup(%q) => %q", n.tree, name, typ)
+	if err != nil {
+		log.Printf("Failed to get type of %q in tree %q: %v", name, n.tree, err)
+		return nil, syscall.EIO
+	}
+	switch typ {
+	case "blob":
+		cmd := exec.Command("git", "-C", n.conf.Git.GitRepo, "cat-file", "-p", n.tree+":"+name)
+		outData, err := cmd.CombinedOutput()
+		if err != nil {
+			log.Printf("Failed to get contents of %q in tree %q: %v", name, n.tree, err)
+			return nil, syscall.EIO
+		}
+		in := n.Inode.NewInode(ctx, &memFile{
+			contents: outData,
+		}, fs.StableAttr{
+			Mode: fuse.S_IFREG | 0644, // TODO: proper mode
+		})
+		return in, 0
+	case "tree":
+		cmd := exec.Command("git", "-C", n.conf.Git.GitRepo, "rev-parse", n.tree+":"+name)
+		outData, err := cmd.CombinedOutput()
+		if err != nil {
+			log.Printf("Failed to get tree hash of %q in tree %q: %v", name, n.tree, err)
+			return nil, syscall.EIO
+		}
+		return n.Inode.NewInode(ctx, &treeNode{
+			conf: n.conf,
+			tree: strings.TrimSpace(string(outData)),
+		}, fs.StableAttr{
+			Mode: fuse.S_IFDIR | 0755,
+		}), 0
+	}
+
+	log.Printf("Unknown type %q for %q in tree %q", typ, name, n.tree)
+	return nil, syscall.ENOENT
+}
+
 // This demonstrates how to build a file system in memory. The
 // read/write logic for the file is provided by the MemRegularFile type.
 func main() {
+	gitCache := filepath.Join(os.Getenv("HOME"), ".cache", "gomodfs")
+	if err := os.MkdirAll(gitCache, 0755); err != nil {
+		log.Panicf("Failed to create git cache directory %s: %v", gitCache, err)
+	}
+	cmd := exec.Command("git", "init", gitCache)
+	cmd.Dir = gitCache
+	cmd.Run() // best effort
+
+	d := &modgit.Downloader{GitRepo: gitCache}
+
 	mntDir := filepath.Join(os.Getenv("HOME"), "mnt-gomodfs")
 	exec.Command("umount", mntDir).Run() // best effort
 
@@ -113,7 +179,13 @@ func main() {
 		log.Panicf("Failed to create mount directory %s: %v", mntDir, err)
 	}
 
-	root := &moduleNameNode{}
+	conf := &config{
+		Git: d,
+	}
+
+	root := &moduleNameNode{
+		conf: conf,
+	}
 	server, err := fs.Mount(mntDir, root, &fs.Options{
 		MountOptions: fuse.MountOptions{Debug: true},
 	})

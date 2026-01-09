3a1301dc2af4119f9f691360eb806f60b71a9d5c
Tom Proctor <tomhjp@users.noreply.github.com>
2026-01-09 17:16:45 +0000

    .github: add go mod download test
    Signed-off-by: Tom Proctor <tomhjp@users.noreply.github.com>

--- gomodfs_test.go
+++ gomodfs_test.go
@@ -5,17 +5,22 @@ package gomodfs
 
 import (
 	"fmt"
+	"net"
 	"net/http"
 	"os"
+	"os/exec"
+	"path/filepath"
 	"strings"
 	"testing"
 	"time"
 
 	"github.com/tailscale/gomodfs/store"
 	"github.com/tailscale/gomodfs/store/gitstore"
+	"github.com/tailscale/gomodfs/temp-dev-fork/willscott/go-nfs"
+	"github.com/tailscale/gomodfs/testing/nfsmount/mount"
 )
 
-// test for https://github.com/tailscale/gomodfs/issues/15a
+// test for https://github.com/tailscale/gomodfs/issues/15
 func TestExoticZip(t *testing.T) {
 	gitCacheDir := testGitDir(t)
 	defer func() {
@@ -69,4 +74,50 @@ entry[2]: upload_file.txt, -rw-r--r--, size=9
 	if got != want {
 		t.Fatalf("bad directory entries; got:\n%s\nwant:\n%s", got, want)
 	}
+}
+
+func TestGoModDownload(t *testing.T) {
+	gitCacheDir := testGitDir(t)
+	st := &gitstore.Storage{GitRepo: gitCacheDir}
+	addStopGitStoreCleanup(t, st)
+	fs := &FS{
+		Store: st,
+		Client: &http.Client{
+			Transport: testDataTransport{},
+		},
+		Logf: t.Logf,
+	}
+	ln, err := net.Listen("tcp", ":0")
+	if err != nil {
+		t.Fatalf("Failed to listen on NFS port %s: %v", ":0", err)
+	}
+	port := ln.Addr().(*net.TCPAddr).Port
+	t.Cleanup(func() {
+		ln.Close()
+	})
+	nfsListenAddr := ln.Addr()
+	t.Logf("NFS server listening at %s", nfsListenAddr)
+	nfsSrv := &nfs.Server{
+		Context: t.Context(),
+		Handler: fs.NFSHandler(),
+	}
+	go nfsSrv.Serve(ln)
+
+	dir := mount.Mount(port)
+	t.Cleanup(func() {
+		cmd := exec.Command("sudo", "umount", "-l", dir)
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			t.Fatalf("umount %s failed: %v\nOutput:\n%s", dir, err, out)
+		}
+	})
+	cmd := exec.CommandContext(t.Context(), "go", "mod", "verify")
+	cmd.Env = append(os.Environ(), "GOMODCACHE="+dir)
+	wd, _ := os.Getwd()
+	cmd.Dir = filepath.Join(wd, "testdata", "exoticmod")
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Fatalf("go mod download failed: %v\nOutput:\n%s", err, out)
+	}
+	t.Logf("go mod download output:\n%s", out)
 }
